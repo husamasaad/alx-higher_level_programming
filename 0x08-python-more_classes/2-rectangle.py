1d042083d44760f4d67eebe87f8c7c9dfb806b82
Husam Asaad <101347296+husamasaad@users.noreply.github.com>
2023-05-01 05:01:53 +0200

    more classes

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 """
-Defines a Rectangle
+Defines a class Rectangle
 """
 
 
 class Rectangle:
-    """representation of a rectangle"""
+    """Representation of a rectangle"""
     def __init__(self, width=0, height=0):
+        """Initializes the rectangle"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Getter for width"""
+        """getter for the private instance attribute width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """setter for width"""
+        """setter for the private instance attribute width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -26,12 +27,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """Getter for height"""
+        """getter for the private instance attribute height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """setter for height"""
+        """setter for the private instance attribute height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -39,11 +40,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Returns: rectangle area"""
+        """returns the area of the rectangle"""
         return self.__width * self.__height
-    
+
     def perimeter(self):
-        """Returns rectangle perimeter"""
-        if self.__width == 0 or self.height == 0:
+        """returns the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__width + self.__height)
+        return (self.__width * 2) + (self.__height * 2)
