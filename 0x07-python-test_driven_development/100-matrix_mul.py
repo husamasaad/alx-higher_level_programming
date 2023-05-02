d13686286123cacdfc68270eec0aa836a53f5438
Husam Asaad <101347296+husamasaad@users.noreply.github.com>
2023-05-02 09:11:15 +0200

    test-driven develompent

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -56,16 +56,16 @@ def matrix_mul(m_a, m_b):
     mul_matrix = []
 
     for row_1 in m_a:
-        l = 0
+        index = 0
         l_row = []
-        while l < len(m_b[0]):
+        while index < len(m_b[0]):
             result = 0
             k = 0
             for column_1 in row_1:
-                result += column_1 * m_b[k][l]
+                result += column_1 * m_b[k][index]
                 k += 1
             l_row.append(result)
-            l += 1
+            index += 1
         mul_matrix.append(l_row)
 
     return mul_matrix
